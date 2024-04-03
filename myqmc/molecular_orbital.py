f18b298b8374289cd320c854ba20ece9b69d2c3b
kousuke-nakano <kousuke.kyo.u@gmail.com>
2024-04-03 14:52:02 +0900

    MO WIP

--- myqmc/molecular_orbital.py
+++ myqmc/molecular_orbital.py
@@ -10,7 +10,7 @@ import numpy.typing as npt
 from logging import getLogger, StreamHandler, Formatter
 
 # myqmc module
-from atomic_orbital import AO
+from atomic_orbital import AO_data, compute_AO
 
 logger = getLogger("myqmc").getChild(__name__)
 
@@ -27,40 +27,31 @@ class MO_data:
     """
 
     coefficients: list[float | complex] = field(default_factory=list)
-    atomic_orbitals: list[AO] = field(default_factory=list)
+    ao_data_l: list[AO_data] = field(default_factory=list)
 
 
-class MO:
+def compute_MO(mo_data: MO_data, r_cart: list[float]) -> float:
     """
     The class contains information for computing a molecular orbital. Just for testing purpose.
-    For fast computations, use MOs_data and MOs.
+    For fast computations, use MOs_data and compute_MOs.
 
     Args:
         mo_data (MO_data): an instance of MO_data
+        r_cart: Cartesian coordinate of an electron
+
+    Returns:
+        Value of the MO value at r_cart.
     """
 
-    def __init__(self, mo_data: MO_data):
-        self.mo_data = mo_data
-
-    def compute(self, r_cart: list[float]) -> float:
-        """
-        Compute the value of the MO at r_cart
-
-        Args:
-            r_cart: Cartesian coordinate of an electron
-
-        Returns:
-            Value of the MO value at r_cart.
-        """
-        return np.inner(
-            np.array(self.mo_data.coefficients),
-            np.array(
-                [
-                    ao_instance.compute(r_cart)
-                    for ao_instance in self.mo_data.atomic_orbitals
-                ]
-            ),
-        )
+    return np.inner(
+        np.array(mo_data.coefficients),
+        np.array(
+            [
+                compute_AO(ao_data=ao_data, r_cart=r_cart)
+                for ao_data in mo_data.ao_data_l
+            ]
+        ),
+    )
 
 
 if __name__ == "__main__":
@@ -71,11 +62,3 @@ if __name__ == "__main__":
     handler_format = Formatter("%(name)s - %(levelname)s - %(lineno)d - %(message)s")
     stream_handler.setFormatter(handler_format)
     log.addHandler(stream_handler)
-
-    exponents = [13.0, 5.0, 1.0]
-    coefficients = [0.001, 0.01, -1.0]
-
-    ao_data = AO_data(exponents=exponents, coefficients=coefficients)
-    ao_sphe = AO(ao_data=ao_data)
-    r_cart = [0.0, 0.0, 1.0]
-    print(ao_sphe.compute(r_cart=r_cart))
